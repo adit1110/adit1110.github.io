5eedf97856824c419aac5b4ba6b6bc87cf0b88b7
Adit <aditbhimani007@gmail.com>
2024-05-05 21:40:24 -0400

    Trying to take out the SVG logo trial thing

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-s.png' /* change this to logo from midget */
 import './index.scss'
 
